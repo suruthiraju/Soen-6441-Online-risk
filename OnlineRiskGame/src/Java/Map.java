9d420e698f0fa10830530a9528ff7c75ad00b329
SuruthiRaju <suruthi25@gmail.com>
2018-09-26 17:08:28 -0400

    Skeleton of workspace, document and Implementation of Junit

---
+++ OnlineRiskGame/src/Java/Map.java
@@ -0,0 +1,19 @@
+package Java;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+
+public class Map {
+	@Ignore
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Map");
+	}
+	@Test
+	public void testAdd() {
+	      String str = "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	   }
+
+}
---
+++ OnlineRiskGame/src/Junit/TestRunner.java
@@ -0,0 +1,12 @@
+package Junit;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import Java.Map;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Map.class })
+public class TestRunner {
+	
+}
