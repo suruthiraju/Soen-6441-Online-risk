23c582acaa3872fa668df90809b2a47c4b6673ad
SuruthiRaju <suruthi25@gmail.com>
2018-10-11 20:18:26 -0400

    reading file changes v1

--- OnlineRiskGame/src/app/model/ContinentsModel.java
+++ OnlineRiskGame/src/app/model/ContinentsModel.java
@@ -1,54 +1,6 @@
-<<<<<<< .mine
 package app.model;
-
-public class ContinentsModel {
-
-	private String continentName;
-	private int valueControl;
-	
-	public ContinentsModel(String continentName, int valueControl ) {
-		this.continentName = continentName;
-		this.valueControl = valueControl;
-	}
-	
-	/**
-	 * 
-	 * @return the continent name.
-	 */
-	public String getContinentName() {
-		return continentName;
-	}
-	
-	/**
-	 * Sets the continent name.
-	 * @param continentName
-	 */
-	public void setContinentName(String continentName) {
-		this.continentName = continentName;
-	}
-	
-	/**
-	 * 
-	 * @return the value Control.
-	 */
-	public int getValueControl() {
-		return valueControl;
-	}
-	
-	/**
-	 * Sets the value Control.
-	 * @param valueControl
-	 */
-	public void setValueControl(int valueControl) {
-		this.valueControl = valueControl;
-	}
-}
-=======
-package app.model;
-
-public class ContinentsModel {
-
-	private String continentName;
+ public class ContinentsModel {
+ 	private String continentName;
 	private int valueControl;
 	public ContinentsModel(String continentName, int valueControl ) {
 		this.continentName = continentName;
@@ -87,5 +39,3 @@ public class ContinentsModel {
 		this.valueControl = valueControl;
 	}
 }
-
->>>>>>> .theirs
