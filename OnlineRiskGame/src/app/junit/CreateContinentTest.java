f1512eadcd47be994e50196903a4869fffbebc65
rohitsharma06596 <rohit.sharma06596@gmail.com>
2018-10-18 17:17:00 -0400

    Create Continent Update

--- OnlineRiskGame/src/app/junit/CreateContinentTest.java
+++ OnlineRiskGame/src/app/junit/CreateContinentTest.java
@@ -1,5 +1,8 @@
 package app.junit;
 import org.junit.Test;
+
+import app.controller.CreateContinentController;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,9 +11,11 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 
 public class CreateContinentTest {
+	CreateContinentController test;
 	
 	
 	@Test public void testCreateContinentAddButton() {
+		
 		
 		
 	}
