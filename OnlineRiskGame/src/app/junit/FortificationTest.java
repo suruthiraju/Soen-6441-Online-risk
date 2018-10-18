58714ff11ed8298a5a0f7e7d8435c647df949b01
Hamidrezaanvari <anvari.hr@gmail.com>
2018-10-18 12:17:17 -0400

    Added variables and core

--- OnlineRiskGame/src/app/junit/FortificationTest.java
+++ OnlineRiskGame/src/app/junit/FortificationTest.java
@@ -1,7 +1,14 @@
 package app.junit;
 
 import org.junit.Test;
+
+import app.model.ContinentsModel;
+import app.model.CountryModel;
+
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,5 +20,15 @@ public class FortificationTest {
 	int a;
 	String nameA;
 	String nameB;
+	ArrayList<ContinentsModel> listOfContinents = new ArrayList<ContinentsModel>();
+	ArrayList<CountryModel> listOfCountrys = new ArrayList<CountryModel>();
+	
+	 
+	
+	@Test public void testFortificationMovingArmies() {
+		
+		
+	}
 
+	
 }
