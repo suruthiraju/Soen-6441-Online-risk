cb854415d8fe36a1ad445f71855f6381048a92a0
Hamidrezaanvari <anvari.hr@gmail.com>
2018-10-19 04:08:00 -0400

    Update OnlineRiskGame/src/app/controller/WelcomeScreenController.java
    Edited javadoc

--- OnlineRiskGame/src/app/controller/WelcomeScreenController.java
+++ OnlineRiskGame/src/app/controller/WelcomeScreenController.java
@@ -10,17 +10,15 @@ import app.view.WelcomeScreenView;
 //between the View and Model
 
 /**
- * 
- * @author DELL
- *
+ * "WelcomeScreenController" is the main class. 
+ * It represents a welcome window containing main buttons to edit/create
+ * map file and start playing game
+ * @author GROUP-35
  */
-
-
 public class WelcomeScreenController implements ActionListener {
 
 	public static void main(String[] args) {
 		WelcomeScreenController wsc = new WelcomeScreenController();
-
 	}
 
 	private WelcomeScreenView theView;
@@ -36,27 +34,27 @@ public class WelcomeScreenController implements ActionListener {
 
 	}
 
+/**
+ * Sets actions to clicked buttons	
+ */
 	@Override
 	public void actionPerformed(ActionEvent actionEvent) {
 		if (actionEvent.getSource().equals(theView.createMapButton)) {
 			// open new game window
 			this.showCreateMapWindow();
-
 		} else if (actionEvent.getSource().equals(theView.editMapButton)) {
 			// open load game window
 			this.showEditGameWindow();
-
 		} else if (actionEvent.getSource().equals(theView.playMapButton)) {
 			// open create game window
 			this.showPlayGameWindow();
-
 		} else if (actionEvent.getSource().equals(theView.exitButton)) {
 			// exit game
 			this.exitGame();
 		}
-
 	}
 
+	
 	private void exitGame() {
 		this.theView.dispose();
 	}
