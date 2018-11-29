a6446332ae031e812ba6bc5456f1cd798c9267c5
SuruthiRaju <suruthi25@gmail.com>
2018-11-29 02:35:37 -0500

    cheater

--- OnlineRiskGame/src/app/controller/CheaterPlayerController.java
+++ OnlineRiskGame/src/app/controller/CheaterPlayerController.java
@@ -1,21 +1,8 @@
 package app.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
-
-import app.model.CardModel;
 import app.model.CountryModel;
 import app.model.GamePlayModel;
-import app.model.PlayerModel;
 import app.utilities.Validation;
-import app.view.AttackView;
-import app.view.FortificationView;
-import app.view.ReinforcementView;
 
 /**
  * In PlayerController, the data flow into model object and updates the view
@@ -25,28 +12,15 @@ import app.view.ReinforcementView;
  * @version 1.0.0
  *
  */
-public class CheaterPlayerController implements ActionListener, ItemListener {
+public class CheaterPlayerController {
 
 	/** The game play model. */
 	private GamePlayModel gamePlayModel;
 
-	/** The reinforcement view. */
-	private ReinforcementView theReinforcementView;
-
-	/** The fortification view. */
-	private FortificationView theFortificationView;
-
-	/** The attack view. */
-	private AttackView theAttackView;
-
-	/** The list of players. */
-	private ArrayList<PlayerModel> listOfPlayers = new ArrayList<PlayerModel>();
-
-	/** The no of players. */
-	private int noOfPlayers;
-
 	/** The val. */
 	private Validation val = new Validation();
+
+	private int index1, index2;
 
 	/**
 	 * Constructor initializes values and sets the screen too visible.
@@ -56,8 +30,6 @@ public class CheaterPlayerController implements ActionListener, ItemListener {
 	public CheaterPlayerController(GamePlayModel gamePlayModel) {
 
 		this.gamePlayModel = gamePlayModel;
-		this.gamePlayModel.getConsoleText()
-				.append("Initiating reinforcement for " + gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer());
 		reinforcement();
 	}
 
@@ -66,216 +38,115 @@ public class CheaterPlayerController implements ActionListener, ItemListener {
 	 * 
 	 */
 	public void reinforcement() {
-		this.gamePlayModel.getConsoleText().setLength(0);
-		this.gamePlayModel.callObservers();
-		this.gamePlayModel.getConsoleText()
-				.append("Initiating Reinforcement for " + gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer());
 
-		this.gamePlayModel.getGameMap().getPlayerTurn().setremainTroop(this.gamePlayModel.numberOfCountries()
-				+ this.gamePlayModel.continentCovered(gamePlayModel.getGameMap().getPlayerTurn()));
-		if (gamePlayModel.getGameMap().getPlayerTurn().getOwnedCards().size() > 0) {
-			this.gamePlayModel.getConsoleText().append("\n Reinforcement View - Please find the list of Cards: \n");
-			for (int i = 0; i < gamePlayModel.getGameMap().getPlayerTurn().getOwnedCards().size(); i++) {
-				this.gamePlayModel.getConsoleText()
-						.append(gamePlayModel.getGameMap().getPlayerTurn().getOwnedCards().get(i).getCardId() + "\n ");
+		for (int j = 0; j < this.gamePlayModel.getPlayers().size(); j++) {
+			if (this.gamePlayModel.getPlayers().get(j).getNamePlayer()
+					.equals(this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer())) {
+				for (int i = 0; i < this.gamePlayModel.getPlayers().get(j).getOwnedCountries().size(); i++) {
+					this.gamePlayModel.getPlayers().get(j).getOwnedCountries().get(i).setArmies(
+							this.gamePlayModel.getPlayers().get(j).getOwnedCountries().get(i).getArmies() * 2);
+				}
 			}
-			this.gamePlayModel.getGameMap().getPlayerTurn().setShowReinforcementCard(true);
 		}
-		theReinforcementView = new ReinforcementView(this.gamePlayModel);
-		theReinforcementView.setActionListener(this);
-		theReinforcementView.setVisible(true);
-
-		this.gamePlayModel.getGameMap().addObserver(theReinforcementView);
-		this.gamePlayModel.addObserver(theReinforcementView);
-		for (int i = 0; i < noOfPlayers; i++) {
-			this.listOfPlayers.get(i).addObserver(this.theReinforcementView);
+		for (int i = 0; i < this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size(); i++) {
+			this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(i).setArmies(
+					this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(i).getArmies() * 2);
+			for (int j = 0; j < this.gamePlayModel.getGameMap().getCountries().size(); j++) {
+				if (this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(i).getCountryName()
+						.equals(this.gamePlayModel.getGameMap().getCountries().get(j).getCountryName())) {
+					this.gamePlayModel.getGameMap().getCountries().get(j)
+							.setArmies(this.gamePlayModel.getGameMap().getCountries().get(j).getArmies() * 2);
+				}
+			}
 		}
-
+		attack();
 	}
 
 	/**
 	 * This method is called in fortification phase.
 	 */
 	public void fortification() {
-		this.gamePlayModel.getConsoleText().setLength(0);
-		this.gamePlayModel.getConsoleText()
-				.append("Initiating Fortification for " + gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer());
+		index1 = getRandomBetweenRange(1, this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size());
+		index1 = index1 - 1;
 
-		theFortificationView = new FortificationView(this.gamePlayModel);
-		theFortificationView.setActionListener(this);
-		theFortificationView.setItemListener(this);
-		theFortificationView.setVisible(true);
-		this.gamePlayModel.addObserver(this.theFortificationView);
+		index2 = indexRandomvalues();
+		index2 = index2 - 1;
+
+		int armies = (this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index1).getArmies() - 1)*2;
+		if (val.checkIfValidMove(this.gamePlayModel.getGameMap(),
+				this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index1),
+				this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index2))) {
+			this.gamePlayModel.getGameMap().setMovingArmies(armies,
+					this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index1),
+					this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index2));
+		}
 	}
 
 	/**
 	 * This method is called in attack phase.
 	 */
 	public void attack() {
-		this.gamePlayModel.getConsoleText().setLength(0);
-		this.gamePlayModel.getConsoleText()
-				.append("Initiating " + gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer() + "'s attack");
+		index1 = getRandomBetweenRange(1, this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size());
+		index1 = index1 - 1;
 
-		theAttackView = new AttackView(this.gamePlayModel);
-		this.gamePlayModel.setArmyToMoveText(false);
-		this.gamePlayModel.setCardToBeAssigned(false);
-		theAttackView.setActionListener(this);
-		theAttackView.setVisible(true);
-		this.gamePlayModel.deleteObservers();
-		this.gamePlayModel.addObserver(this.theAttackView);
-	}
-
-	/**
-	 * This method performs action, by Listening the action event set in view.
-	 *
-	 * @param actionEvent the action event
-	 */
-	@Override
-	public void actionPerformed(ActionEvent actionEvent) {
-		if (actionEvent.getSource().equals(this.theReinforcementView.addButton)) {
-			int selectedArmies = 0;
-			if (theReinforcementView.numOfTroopsComboBox.getSelectedItem() != null) {
-				selectedArmies = (int) theReinforcementView.numOfTroopsComboBox.getSelectedItem();
-				CountryModel countryName = (CountryModel) theReinforcementView.countryListComboBox.getSelectedItem();
-				System.out.println("countryName" + selectedArmies + countryName);
-				this.gamePlayModel.setSelectedArmiesToCountries(selectedArmies, countryName);
-			} else {
-				this.theReinforcementView.dispose();
-				attack();
-
-			}
-		} else if (actionEvent.getSource().equals(this.theReinforcementView.addMoreButton)) {
-			int cardID = Integer.parseInt(this.theReinforcementView.cardIdField.getText());
-			int cardValue = 0;
-			CardModel card = new CardModel();
-			for (int i = 0; i < this.gamePlayModel.getCards().size(); i++) {
-				if (cardID == this.gamePlayModel.getCards().get(i).getCardId()) {
-					cardValue = this.gamePlayModel.getCards().get(i).getCardValue();
-				}
-			}
-			card.setCardId(cardID);
-			card.setCardValue(cardValue);
-			this.gamePlayModel.getGameMap().getPlayerTurn()
-					.setremainTroop(this.gamePlayModel.getGameMap().getPlayerTurn().getremainTroop() + cardValue);
-			for (int i = 0; i < this.gamePlayModel.getPlayers().size(); i++) {
-				if (gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
-						.equals(gamePlayModel.getPlayers().get(i).getNamePlayer())) {
-					this.gamePlayModel.getPlayers().get(i).removeCard(card);
-				}
-			}
-			this.gamePlayModel.getCards().add(card);
-			this.gamePlayModel.callObservers();
-		} else if (actionEvent.getSource().equals(this.theReinforcementView.exitCardButton)) {
-			for (int i = 0; i < this.gamePlayModel.getPlayers().size(); i++) {
-				if (gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
-						.equals(gamePlayModel.getPlayers().get(i).getNamePlayer())) {
-					if (gamePlayModel.getGameMap().getPlayerTurn().getOwnedCards().size() >= 5) {
-						this.gamePlayModel.getGameMap().getPlayerTurn().setShowReinforcementCard(true);
-						gamePlayModel.getPlayers().get(i).setShowReinforcementCard(true);
-						JOptionPane.showOptionDialog(null,
-								"Maximum 5 card is allowed. Please select card id to reimburse", "Reimburse card",
-								JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {},
-								null);
+		CountryModel attackCountry = this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().get(index1);
+		for (int i = 0; i < attackCountry.getLinkedCountries().size(); i++) {
+			for (int j = 0; j < this.gamePlayModel.getGameMap().getCountries().size(); j++) {
+				if (attackCountry.getLinkedCountries().get(i).getCountryName()
+						.equals(this.gamePlayModel.getGameMap().getCountries().get(j).getCountryName())) {
+					if (this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
+							.equals(this.gamePlayModel.getGameMap().getCountries().get(j).getRulerName())) {
+						continue;
 					} else {
-						this.gamePlayModel.getGameMap().getPlayerTurn().setShowReinforcementCard(false);
-						gamePlayModel.getPlayers().get(i).setShowReinforcementCard(false);
-						this.gamePlayModel.callObservers();
+						for (int k = 0; k < this.gamePlayModel.getPlayers().size(); k++) {
+							if (this.gamePlayModel.getGameMap().getCountries().get(j).getRulerName()
+									.equals(this.gamePlayModel.getPlayers().get(k).getNamePlayer())) {
+								this.gamePlayModel.getPlayers().get(k)
+										.defend(this.gamePlayModel.getGameMap().getCountries().get(j));
+							}
+							if (this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer()
+									.equals(this.gamePlayModel.getPlayers().get(k).getNamePlayer())) {
+								this.gamePlayModel.getPlayers().get(k)
+										.attacked(this.gamePlayModel.getGameMap().getCountries().get(j));
+							}
+						}
+						this.gamePlayModel.getGameMap().getPlayerTurn()
+								.attacked(this.gamePlayModel.getGameMap().getCountries().get(j));
+						this.gamePlayModel.getGameMap().getCountries().get(j)
+								.setRulerName(this.gamePlayModel.getGameMap().getPlayerTurn().getNamePlayer());
 					}
 				}
 			}
-		} else if (actionEvent.getSource().equals(this.theAttackView.nextButton)) {
-			this.theAttackView.dispose();
-			fortification();
-		} else if (actionEvent.getSource().equals(this.theAttackView.attackCountryListComboBox)) {
-			this.gamePlayModel
-					.setSelectedAttackComboBoxIndex(this.theAttackView.attackCountryListComboBox.getSelectedIndex());
-		} else if (actionEvent.getSource().equals(this.theAttackView.defendCountryListComboBox)) {
-			this.gamePlayModel
-					.setSelectedDefendComboBoxIndex(this.theAttackView.defendCountryListComboBox.getSelectedIndex());
-		} else if (actionEvent.getSource().equals(this.theAttackView.SingleButton)) {
-
-			int attackDice = (int) theAttackView.numOfDiceAttackComboBox.getSelectedItem();
-			int defendDice = (int) theAttackView.numOfDiceDefendComboBox.getSelectedItem();
-			CountryModel attackCountry = (CountryModel) theAttackView.attackCountryListComboBox.getSelectedItem();
-			CountryModel defendCountry = (CountryModel) theAttackView.defendCountryListComboBox.getSelectedItem();
-			this.gamePlayModel.setDefeatedCountry(defendCountry);
-			this.gamePlayModel.singleStrike(attackDice, attackCountry, defendDice, defendCountry);
-			if (val.endOfGame(this.gamePlayModel) == true) {
-				JOptionPane.showOptionDialog(null, "Bravo! You have won! Game is over!", "Valid",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {}, null);
-				this.theAttackView.dispose();
-			}
-
-		} else if (actionEvent.getSource().equals(this.theAttackView.alloutButton)) {
-
-			CountryModel attackCountry = (CountryModel) theAttackView.attackCountryListComboBox.getSelectedItem();
-			CountryModel defendCountry = (CountryModel) theAttackView.defendCountryListComboBox.getSelectedItem();
-			this.gamePlayModel.setDefeatedCountry(defendCountry);
-			this.gamePlayModel.alloutStrike(attackCountry, defendCountry);
-			if (val.endOfGame(this.gamePlayModel) == true) {
-				JOptionPane.showOptionDialog(null, "Bravo! You have won! Game is over!", "Valid",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {}, null);
-				this.theAttackView.dispose();
-			}
-
-		} else if (actionEvent.getSource().equals(this.theAttackView.moveButton)) {
-
-			CountryModel attackCountry = (CountryModel) theAttackView.attackCountryListComboBox.getSelectedItem();
-			int noOfArmiesToBeMoved = (int) theAttackView.numOfArmiesToBeMovedComboBox.getSelectedItem();
-			CountryModel defendCountry = this.gamePlayModel.getDefeatedCountry();
-			this.gamePlayModel.moveArmies(attackCountry, defendCountry, noOfArmiesToBeMoved);
-
-		} else if (actionEvent.getSource().equals(this.theFortificationView.moveButton)) {
-			// BFS
-
-			if (val.checkIfValidMove(this.gamePlayModel.getGameMap(),
-					(CountryModel) this.theFortificationView.fromCountryListComboBox.getSelectedItem(),
-					(CountryModel) this.theFortificationView.toCountryListComboBox.getSelectedItem())) {
-				this.gamePlayModel.getGameMap().setMovingArmies(
-						(Integer) this.theFortificationView.numOfTroopsComboBox.getSelectedItem(),
-						(CountryModel) this.theFortificationView.fromCountryListComboBox.getSelectedItem(),
-						(CountryModel) this.theFortificationView.toCountryListComboBox.getSelectedItem());
-			}
-
-			int index = this.gamePlayModel.getGameMap().getPlayerIndex();
-
-			this.gamePlayModel.moveDeck();
-
-			index++;
-			if (this.gamePlayModel.getPlayers().size() > index) {
-				this.gamePlayModel.getGameMap().setPlayerIndex(index);
-				this.gamePlayModel.getPlayers().get(index).callObservers();
-			} else {
-				index = 0;
-				this.gamePlayModel.getGameMap().setPlayerIndex(index);
-				this.gamePlayModel.getPlayers().get(index).callObservers();
-			}
-			if (val.endOfGame(this.gamePlayModel) == false) {
-				new GamePlayController(this.gamePlayModel);
-				this.theFortificationView.dispose();
-			} else {
-				JOptionPane.showOptionDialog(null, "Bravo! You have won! Game is over!", "Valid",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {}, null);
-				this.theFortificationView.dispose();
-			}
-		} else if (actionEvent.getSource().equals(this.theFortificationView.fromCountryListComboBox)) {
-			this.gamePlayModel
-					.setSelectedComboBoxIndex(this.theFortificationView.fromCountryListComboBox.getSelectedIndex());
 		}
 
+		fortification();
 	}
+	
+	public int indexRandomvalues() {
+		boolean flag = false;
+		index2 = getRandomBetweenRange(1, this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size());
 
-	/**
-	 * Item Listener.
-	 *
-	 * @param itemEvent the item event
-	 * @see java.awt.event.ItemListener#itemStateChanged(java.awt.event.ItemEvent)
-	 */
-	public void itemStateChanged(ItemEvent itemEvent) {
-		if (itemEvent.getSource().equals(this.theFortificationView.fromCountryListComboBox)) {
-			this.gamePlayModel
-					.setSelectedComboBoxIndex(this.theFortificationView.fromCountryListComboBox.getSelectedIndex());
+		if (index1 == index2) {
+			flag = true;
 		}
-
+		while (flag == true) {
+			index2 = getRandomBetweenRange(1,
+					this.gamePlayModel.getGameMap().getPlayerTurn().getOwnedCountries().size());
+			if (index1 != index2) {
+				flag = false;
+			}
+		}
+		return index2;
+	}
+	/**
+	 * This method gives the Random generation of numbers within two values.
+	 *
+	 * @param min the min
+	 * @param max the max
+	 * @return the random between range
+	 */
+	public int getRandomBetweenRange(double min, double max) {
+		int x = (int) ((Math.random() * ((max - min) + 1)) + min);
+		return x;
 	}
 }
